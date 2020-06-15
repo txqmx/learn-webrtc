4d155ef8ab58283085e68c813caf903d88c0cef9
liusinan <liusinan@100tal.com>
2020-06-15 11:32:06 +0800

    音视频获取

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
 import App from './App.vue';
+import 'element-ui/lib/theme-chalk/index.css';
 import './registerServiceWorker';
 import router from './router';
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/videoRecord.vue
+++ src/views/videoRecord.vue
@@ -13,7 +13,7 @@
 
 <script>
 export default {
-  name: 'record',
+  name: 'videoRecord',
   mounted() {
     this.getUserMedia();
   },
