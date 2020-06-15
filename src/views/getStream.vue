5dbfe77586a30e7134ffd33c142ff529a204f7ee
liusinan <liusinan@100tal.com>
2020-06-15 11:31:14 +0800

    音视频获取

---
+++ src/views/getStream.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <div>
+      <div class="deviceInfo">
+        <label>audioInput：</label>
+        <el-select v-model="audioInputValue" size="mini" placeholder="请选择">
+          <el-option
+            v-for="item in audioInput"
+            :key="item.deviceId"
+            :label="item.label"
+            :value="item.deviceId">
+          </el-option>
+        </el-select>
+      </div>
+      <div class="deviceInfo">
+        <label>audioOutput：</label>
+        <el-select v-model="audioOutputValue" size="mini" placeholder="请选择">
+          <el-option
+            v-for="item in audioOutput"
+            :key="item.deviceId"
+            :label="item.label"
+            :value="item.deviceId">
+          </el-option>
+        </el-select>
+      </div>
+      <div class="deviceInfo">
+        <label>videoInput：</label>
+        <el-select v-model="videoInputValue" size="mini" placeholder="请选择">
+          <el-option
+            v-for="item in videoInput"
+            :key="item.deviceId"
+            :label="item.label"
+            :value="item.deviceId">
+          </el-option>
+        </el-select>
+      </div>
+    </div>
+    <div>
+      <video ref="player" autoplay playsinline ></video>
+      <div style="display: inline-block">{{videoMsg}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  mounted() {
+    this.getDevices();
+    this.getUserMedia();
+  },
+  data() {
+    return {
+      audioInputValue: '',
+      audioOutputValue: '',
+      videoInputValue: '',
+      audioInput: [],
+      audioOutput: [],
+      videoInput: [],
+      videoStream: '',
+      videoMsg: '',
+    };
+  },
+  methods: {
+    // 获取音视频设备信息
+    getDevices() {
+      navigator.mediaDevices.enumerateDevices().then((deviceInfos) => {
+        deviceInfos.forEach((deviceInfo) => {
+          if (deviceInfo.kind === 'audioinput') {
+            this.audioInput.push(deviceInfo);
+          } else if (deviceInfo.kind === 'audiooutput') {
+            this.audioOutput.push(deviceInfo);
+          } else if (deviceInfo.kind === 'videoinput') {
+            this.videoInput.push(deviceInfo);
+          }
+        });
+      });
+    },
+    // 音视频采集
+    getUserMedia() {
+      const MediaDevices = { // 对音视频限制
+        // video: true,
+        video: {
+          width: 320,
+          height: 240,
+          frameRate: 18,
+        },
+        audio: {
+          noiseSuppression: true,
+        },
+      };
+      const { player } = this.$refs;
+      navigator.mediaDevices.getUserMedia(MediaDevices).then((stream) => {
+        const videoTrack = stream.getVideoTracks()[0]; // 获取视频轨道
+        this.videoMsg = videoTrack.getSettings();
+        player.srcObject = stream;
+      });
+    },
+  },
+
+};
+</script>
+
+<style lang="scss">
+.deviceInfo{
+  margin: 10px;
+  label{
+    display: inline-block;
+    width: 150px;
+  }
+}
+</style>
---
+++ src/views/videoRecord.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div>
+      <video ref="player" autoplay playsinline></video>
+      <el-button @click="startRecord">开始录制</el-button>
+      <el-button @click="stopRecord">停止录制</el-button>
+      <el-button @click="mediaPlay">播放</el-button>
+      <el-button @click="downLoad">下载</el-button>
+    </div>
+    <video playsinline ref="replay"></video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'record',
+  mounted() {
+    this.getUserMedia();
+  },
+  data() {
+    return {
+      videoBuffer: [],
+      audioBuffer: [],
+      stream: '',
+      mediaRecorder: '',
+    };
+  },
+  methods: {
+    // 音视频采集
+    getUserMedia() {
+      const MediaDevices = { // 对音视频限制
+        // video: true,
+        video: {
+          width: 320,
+          height: 240,
+          frameRate: 18,
+        },
+        audio: {
+          noiseSuppression: true,
+        },
+      };
+      const { player } = this.$refs;
+      navigator.mediaDevices.getUserMedia(MediaDevices).then((stream) => {
+        this.stream = stream;
+        player.srcObject = stream;
+      });
+    },
+    // 开始录制
+    startRecord() {
+      this.videoBuffer = [];
+      const options = {
+        mimeType: 'video/webm;codecs=vp8',
+      };
+      if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+        console.error(`${options.mimeType} is not supported!`);
+      }
+      const mediaRecorder = new MediaRecorder(this.stream, options);
+      this.mediaRecorder = mediaRecorder;
+      mediaRecorder.ondataavailable = (e) => {
+        if (e && e.data && e.data.size > 0) {
+          this.videoBuffer.push(e.data);
+        }
+      };
+      mediaRecorder.start(10);
+    },
+    // 停止录制
+    stopRecord() {
+      this.mediaRecorder.stop();
+    },
+    // 下载
+    downLoad() {
+      const blob = new Blob(this.videoBuffer, { type: 'video/webm' });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.style.display = 'none';
+      a.download = 'aaa.webm';
+      a.click();
+    },
+    // 播放
+    mediaPlay() {
+      const { replay } = this.$refs;
+      const blob = new Blob(this.videoBuffer, { type: 'video/webm' });
+      replay.src = window.URL.createObjectURL(blob);
+      replay.srcObject = null;
+      replay.controls = true;
+      replay.play();
+    },
+  },
+
+};
+</script>
+
+<style lang="scss">
+
+</style>
